26dc2e208d31bf6c871f5d408db3cc158b28af95
nask <ben@nask.fr>
2026-02-01 02:07:36 +0100

    First one.

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+export default defineConfig({
+  site: "https://naskz.github.io/",
+  integrations: [mdx(), sitemap(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
