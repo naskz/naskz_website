1d3c73b826c1f68798924ad02083a7cbdbfc5be2
nask <ben@nask.fr>
2026-02-01 03:08:23 +0100

    Base URL correct.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://naskz.github.io",
-  base: '/',
+  base: '/naskz_website',
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
